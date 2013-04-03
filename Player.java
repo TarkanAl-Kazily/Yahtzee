c4ab3c8e79c7fea63acd80fb7dec97f28810e1e6
S <s>
2013-04-03 14:23:44 -0700

    {"type":"TI", "runs":"0"}

--- Player.java
+++ Player.java
@@ -18,7 +18,6 @@ public class Player {
 	}
 	
 	private void sort(int[] arr) {
-		int max = arr[0];
 		for(int i = 0; i < arr.length; i++) {
 			int test = arr[i];
 			for(int j = 0; j < arr.length; j++) {
