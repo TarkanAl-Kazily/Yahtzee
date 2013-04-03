1f0a967974b03ef1a93b9676e02f915f25f79a40
S <s>
2013-04-03 15:29:42 -0700

    {"type":"TI", "runs":"0"}

--- Player.java
+++ Player.java
@@ -52,7 +52,7 @@ public class Player {
 		for(int i = 0; i < arr.length; i++) {
 			int counter = 0;
 			for(int j = 0; j < arr.length; j++) {
-				
+				if (arr[i] == arr[j]) count++;
 			}
 		}
 	}
