cd54abd1c43167a2431434552b9a5b0d5c7f36a8
S <s>
2013-04-03 20:42:14 -0700

    {"type":"TI", "runs":"0"}

--- Player.java
+++ Player.java
@@ -72,6 +72,7 @@ public class Player {
 		for(int i = 0; i < (arr.length - 2); i++) {
 			if ((arr[i] == (arr[i+1] + 1)) && (arr[i] == (arr[i+2] + 2))) return true;
 		}
+		return false;
 	}
 	
 	private boolean fullHouse(int[] arr) {
