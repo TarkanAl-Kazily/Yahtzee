f683fd0740782f655b0f18679d13b9b637e7a905
S <s>
2013-04-03 20:41:49 -0700

    {"type":"TI", "runs":"0"}

--- Player.java
+++ Player.java
@@ -69,7 +69,9 @@ public class Player {
 	}
 	
 	private boolean smallStraight(int[] arr) {
-		
+		for(int i = 0; i < (arr.length - 2); i++) {
+			if ((arr[i] == (arr[i+1] + 1)) && (arr[i] == (arr[i+2] + 2))) return true;
+		}
 	}
 	
 	private boolean fullHouse(int[] arr) {
