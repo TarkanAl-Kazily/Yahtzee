263a349e779cfce210d42e980325a557a150bcf0
S <s>
2013-04-04 10:22:31 -0700

    {"type":"TI", "runs":"0"}

--- Player.java
+++ Player.java
@@ -78,6 +78,10 @@ public class Player {
 		else return 0;
 	}
 	
+	private boolean yahtzee(int[] arr) {
+		
+	}
+	
 	private boolean smallStraight(int [] arr) {
 		for(int i = 0; i < (arr.length - 3); i++) {
 			if ((arr[i] == (arr[i+1] - 1)) && (arr[i] == (arr[i+2] - 2)) && (arr[i] == (arr[i+3] - 3))) return true;
