a986487f78d06e388cea2ba25fda600dab2241c2
S <s>
2013-04-04 12:00:52 -0700

    {"type":"TI", "runs":"1"}

--- Player.java
+++ Player.java
@@ -116,31 +116,8 @@ public class Player {
 	}
 	
 	private boolean fullHouse(int[] arr) {
-		boolean three = false;
-		boolean two = false;
-		int numThree = arr[0];
-		for(int i = 0; i < arr.length; i++) {
-			int counter = 0;
-			for(int j = 0; j < arr.length; j++) {
-				if (arr[i] == arr[j] && i != j) counter++;
-			}
-			if (counter >= 3) {
-				three = true;
-				numThree = arr[i];
-				break;
-			}
-		}
-		for(int i = 0; i < arr.length; i++) {
-			int counter = 0;
-			for(int j = 0; j < arr.length; j++) {
-				if (arr[i] == arr[j] && i != j) counter++;
-				if (counter >= 2 && arr[i] != numThree) {
-					two = true;
-					break;
-				}
-			}
-		}
-		return three && two;
+		
+		
 	}
 	
 	private boolean numOfAKind(int[] arr, int num) {
