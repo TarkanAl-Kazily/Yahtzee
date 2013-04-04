88f065ebd1bf93e26dafbf62c84a115208ef1a0b
S <s>
2013-04-04 10:26:39 -0700

    {"type":"TI", "runs":"0"}

--- Player.java
+++ Player.java
@@ -147,8 +147,8 @@ public class Player {
 	
 	private int totalArr(int[] arr) {
 		int result = 0;
-		for(int i = 0; i < arr.length; i++) {
-			result += arr[i];
+		for(int i : arr) {
+			result += i;
 		}
 		return result;
 	}
