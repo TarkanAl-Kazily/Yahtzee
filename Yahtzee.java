aa4240fae390d615d80cce9f1dcc1fd63ac4d166
S <s>
2013-04-04 13:40:41 -0700

    {"type":"TI", "runs":"0"}

--- Yahtzee.java
+++ Yahtzee.java
@@ -80,7 +80,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
  			category = display.waitForPlayerToSelectCategory();
  		}
  		display.updateScorecard(category, turn, (players[turn - 1].getPoints(category)));
- 		display.updateScoreCard(YahtzeeConstants.TOTAL, turn, (players[turn - 1].getPoints(YahtzeeConstants.TOTAL)));
+ 		display.updateScorecard(YahtzeeConstants.TOTAL, turn, (players[turn - 1].getPoints(YahtzeeConstants.TOTAL)));
 	}
 	
 /* Private instance variables */
