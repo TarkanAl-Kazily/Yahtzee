844a81e02fb8d2bd3bfadbdf5c9613c26770da85
S <s>
2013-04-04 13:37:18 -0700

    {"type":"TI", "runs":"0"}

--- Yahtzee.java
+++ Yahtzee.java
@@ -65,6 +65,7 @@ public class Yahtzee extends GraphicsProgram implements YahtzeeConstants {
  		display.waitForPlayerToClickRoll(turn);
  		int[] dice = getDice();
  		display.displayDice(dice);
+ 		display.printMessage("Select the dice you wish to re-roll and click \"Roll Again\".");
  		display.waitForPlayerToSelectDice();
  		dice = rerollDice(dice);
  		display.displayDice(dice);
